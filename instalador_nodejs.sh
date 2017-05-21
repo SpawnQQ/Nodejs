8688321d860fd86745774309ae09015858fb4420
Nicolas Aguirre T <nick.aguirre.16@gmail.com>
2017-05-21 13:53:17 -0400

    Se actualizo el instalador

--- instalador_nodejs.sh
+++ instalador_nodejs.sh
@@ -1,3 +1,5 @@
+sudo apt-get install curl
 echo -e "Fuente: https://nodejs.org/es/download/package-manager/"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get update && apt-get upgrade
 sudo apt-get install -y nodejs
